fa22108c7b7cac87d0b40f22cb8073953a634042
Delancy <adrian.p.delancy@accenture.com>
2016-12-29 09:38:08 -0500

    completed calculate cluster datastore slots function

--- get_vSphere_data.py
+++ get_vSphere_data.py
@@ -1,18 +1,11 @@
+from datetime import timedelta, datetime
 import atexit
-import argparse
-import getpass
 import ssl
-import unittest
-from datetime import timedelta
 
 from pyvim import connect
 from pyVmomi import vmodl
 from pyVmomi import vim
-
-host = '10.12.30.20'
-user = 'splab\captest'
-password = 'Password123!'
-port = 443
+import numpy as np
 
 
 class VcenterApi(object):
@@ -63,27 +56,27 @@ class VcenterApi(object):
     def get_all_vms(self):
         return self.get_all_vms_view().view
 
-    def get_vm_by_uuid(self, search_index, uuid):
-        return search_index.FindByUuid(None, uuid, True, True)
+    def get_vm_by_uuid(self, uuid):
+        return self.db_conn.content.searchIndex.FindByUuid(None, uuid, True, True)
 
     def get_vm_by_ip(self, search_index, ip_address):
         return search_index.FindByIp(None, ip_address, True)
 
-    def get_vm_perf_stats(self, vm):
+    def get_esxi_host_by_name(self):
         pass
 
-    def build_query(self, vchtime, counterId, instance, vm, interval):
+    def build_perf_query(self, vchtime, counterId, instance, resource_entity, interval):
         perfManager = self.db_conn.content.perfManager
         metricId = vim.PerformanceManager.MetricId(counterId=counterId, instance=instance)
         startTime = vchtime - timedelta(minutes=(interval + 1))
         endTime = vchtime - timedelta(minutes=1)
 
-        query = vim.PerformanceManager.QuerySpec(intervalId=20, entity=vm, metricId=[metricId], startTime=startTime,
-                                                 endTime=endTime)
-        perfResults = perfManager.QueryPerf(querySpec=[query])
+        query = vim.PerformanceManager.QuerySpec(intervalId=20, entity=resource_entity,
+                                                 metricId=[metricId],startTime=startTime, endTime=endTime)
+        perf_results = perfManager.QueryPerf(querySpec=[query])
         try:
-            assert perfResults not in [None, [], '']
-            return perfResults
+            assert perf_results not in [None, [], '']
+            return perf_results
         except AssertionError:
             print('ERROR: Performance results empty.  TIP: Check time drift on source and vCenter server')
             print('Troubleshooting info:')
@@ -119,21 +112,209 @@ class VcenterApi(object):
             total_props += ret_props.objects
         managed_obj.Destroy()
         # Turn the output in retProps into a usable dictionary of values
-        gpOutput = []
-        for eachProp in total_props:
-            propDic = dict()
-            for prop in eachProp.propSet:
-                propDic[prop.name] = prop.val
-            propDic['moref'] = eachProp.obj
-            gpOutput.append(propDic)
-        return gpOutput
+        gp_output = []
+        for each_prop in total_props:
+            prop_dic = dict()
+            for prop in each_prop.propSet:
+                prop_dic[prop.name] = prop.val
+            prop_dic['moref'] = each_prop.obj
+            gp_output.append(prop_dic)
+        return gp_output
 
     def stat_check(self, counter_name):
         counter_key = self.perf_dict[counter_name]
         return counter_key
 
-    def print_vm_details(self, vm, interval=1, vchtime=None):
+    def get_esxi_hosts_info(self):
+        """
+        Returns a view container that has the information on the esxi hosts. Access the view property to get the list
+        of esix hosts.
+        :return:
+        """
+        # Search for all ESXi hosts
         content = self.db_conn.content
+        return content.viewManager.CreateContainerView(content.rootFolder, [vim.HostSystem], True)
+
+    def build_esxi_hosts_capacity_info(self):
+        data = self.get_esxi_hosts_info()
+        out = []
+        # Verify hosts properties
+        for host in data.view:
+            info = {
+                'hostname': host.config.network.dnsConfig.hostName,
+                'cluster': host.parent.name,
+                'domain': host.config.network.dnsConfig.searchDomain[0], # can be the .domainName property also if not blank
+                'vendor': host.summary.hardware.vendor,
+                'model': host.summary.hardware.model,
+                'ram': host.summary.hardware.memorySize / (1024/1024/1024),  # GB
+                'cpu-model': host.summary.hardware.numCpuPkgs,
+                'cpu-cores': host.summary.hardware.numCpuCores,
+                'cpu-threads': host.summary.hardware.numNics,
+                'runtime': host.RetrieveHardwareUptime() / (60 * 60 * 24),
+                'id-info': host.summary.hardware.otherIdentifyingInfo,  # List of id tags, can loop thru to identify service tag
+                'status': host.summary.overallStatus,
+                'cpu-ussage-mhz': host.summary.quickstats.overallCpuUsage,
+                'ram-ussage': host.summary.quickstats.overallMemoryUsage / 1024,  # GB
+                'cpu-capacity': host.summary.hardware.cpuMhz
+            }
+            out.append(info)
+        return out
+
+    def get_host_perf_info(self, dns_name='splabvh7001.splab.accenture.com'):
+        #host = self.get_vm_by_ip(self.db_conn.content.searchIndex, self.vCenter_host)
+        host = self.db_conn.content.searchIndex.FindByDnsName(dnsName=dns_name, vmSearch=False)
+        perfManager = self.db_conn.content.perfManager
+        metricId = vim.PerformanceManager.MetricId(counterId=6, instance="*")
+        startTime = datetime.now() - timedelta(hours=1)
+        endTime = datetime.now()
+
+        query = vim.PerformanceManager.QuerySpec(maxSample=1,
+                                                 entity=host,
+                                                 metricId=[metricId],
+                                                 startTime=startTime,
+                                                 endTime=endTime)
+
+        return perfManager.QueryPerf(querySpec=[query])
+
+    def get_datastore_info(self):
+        # Search for all Datastores hosts
+        content = self.db_conn.content
+        return content.viewManager.CreateContainerView(content.rootFolder, [vim.Datastore], True)
+
+    def build_datastore_capacity_info(self, ds_view_container):
+        output = []
+        epoch = get_epoch_value()
+        for datastore in ds_view_container.view:
+            info = {
+                'name': datastore.name,
+                'capacity': datastore.summary.capacity / 1024 / 1024 / 1024,
+                'freespace': datastore.summary.freespace / 1024 / 1024 / 1024,
+                #'usage': datastore.summary.,
+                'vms': datastore.summary,
+                'commitment': '',
+                'epoch': epoch
+            }
+            output.append(info)
+        return output
+
+    def get_compute_cluster_view(self):
+        content = self.db_conn.content
+        return content.viewManager.CreateContainerView(content.rootFolder, [vim.ClusterComputeResource], True)
+
+    def build_cluster_capacity_info(self):
+        clusters = self.get_compute_cluster_view().view
+        output = []
+        for cluster in clusters:
+            cluster_usage = cluster.GetResourceUsage()
+            #print cluster_usage
+            cluster_info = {
+                'name': cluster.name,
+                'cpuCapacityMHz': cluster.summary.totalCpu,
+                'effectiveCpuMhz': cluster.summary.effectiveCpu,
+                'cpuUsedMHz': cluster_usage.cpuUsedMHz,
+                'memCapacityMB': cluster_usage.memCapacityMB,
+                'memUsedMB': cluster_usage.memUsedMB,
+                'numCpuCores': cluster.summary.numCpuCores,
+                'numCpuThreads': cluster.summary.numCpuThreads,
+                'storageUsedMB': cluster_usage.storageUsedMB,
+                'storageCapacityMB': cluster_usage.storageCapacityMB,
+                'effectiveMemoryMB': cluster.summary.effectiveMemory,  # MB
+                'numHosts': cluster.summary.numHosts,
+                'numEffectiveHosts': cluster.summary.numEffectiveHosts,
+                'overallStatus': cluster.summary.overallStatus,
+            }
+            output.append(cluster_info)
+        return output
+
+    @classmethod
+    def get_cluster_vms(cls, cluster):
+        output = []
+        for host in cluster.host:
+            for vm in host.vm:
+                if vm.config.template is False:
+                    output.append(vm)
+            # output.extend(host.vm)
+        return output
+
+    def get_cpu_slots_available(self, clusters):
+        # 1. Get total CPU MHz for all VM Hosts in cluster
+        #  2. Get total CPU MHz used by VM Hosts in cluster
+        #  3. Calculate 90% of the total cpu mhz
+        #  4. Calculate CPU MHz available by subtracting used from total
+        #  5. Divide Available CPU MHz by average cluster VM CPU usage
+        cluster_cpu_slots = dict()
+        for cluster in clusters:
+            total_eff_cpu = cluster.summary.effectiveCpu * 0.9  # 10% padding
+            total_used_cpu = cluster.GetResourceUsage().cpuUsedMHz
+            total_available_cpu = total_eff_cpu - total_used_cpu
+            if total_available_cpu <= 0:
+                return 0
+            vms = self.get_cluster_vms(cluster)
+            vm_cpu_usage_vals = []
+            for vm in vms:
+                if vm.summary.quickStats.overallCpuDemand != 0:
+                    vm_cpu_usage_vals.append(vm.summary.quickStats.overallCpuDemand)
+            try:
+                avg_vm_cpu_usage = sum(vm_cpu_usage_vals) / len(vm_cpu_usage_vals)
+                slots = total_available_cpu / avg_vm_cpu_usage
+                cluster_cpu_slots[cluster.name] = dict(metric=avg_vm_cpu_usage, unit='MHz', slots=slots,
+                                                       type='cluster-cpu-slots')
+            except ZeroDivisionError as error:
+                print('Cluster has no active VMs ' + error.message)  # Todo: Add logging
+                cluster_cpu_slots[cluster.name] = dict(slots='unlimited', metric=None, unit=None,
+                                                       type='cluster-cpu-slots')
+        return cluster_cpu_slots
+
+    def get_memory_slots_available(self, clusters):
+        cluster_mem_slots = dict()
+        for cluster in clusters:
+            cluster_usage = cluster.GetResourceUsage()
+            total_mem = cluster.summary.effectiveMemory * 0.9  # Pad the capacity for more conservative est
+            total_used_mem = cluster_usage.memUsedMB
+            available_mem = total_mem - total_used_mem
+            # Get the usage across all vms in cluster to calculate average vm usage
+            vms = self.get_cluster_vms(cluster)
+            vms_mem_values_tmp = [x.summary.quickStats.guestMemoryUsage for x in vms]
+            vms_mem_values = filter(lambda mem: mem > 0, vms_mem_values_tmp)
+            try:
+                avg_vm_mem_usage = sum(vms_mem_values) / len(vms_mem_values)  # calculate what a mem slot is
+                slots = available_mem / avg_vm_mem_usage
+                cluster_mem_slots[cluster.name] = dict(slots=slots, metric=avg_vm_mem_usage,
+                                                       unit='MB', type='cluster-mem-slots')
+            except ZeroDivisionError:
+                print('Cluster {0} has no VMs'.format(cluster.name))
+                cluster_mem_slots[cluster.name] = dict(slots='unlimited', metric=None,
+                                                       unit=None, type='cluster-mem-slots')
+        return cluster_mem_slots
+
+    def get_datastore_slots_available(self, clusters):
+        cluster_datastore_slots = dict()
+        for cluster in clusters:
+            usage_info = cluster.GetResourceUsage()
+            total_capacity = usage_info.storageCapacityMB * 0.95
+            used = usage_info.storageUsedMB
+            available = total_capacity - used
+            # Calculate VM average
+            vms = self.get_cluster_vms(cluster)
+            vm_storage_vals = []
+            for vm in vms:  # Todo: comitted + uncomitted
+                used = 0
+                for datastore in vm.storage.perDatastoreUsage:
+                    used = used + datastore.committed + datastore.uncommitted  # Units - Bytes
+                vm_storage_vals.append(used)
+
+            # Calculate average
+            avg_vm_datastore_usage = convert_byte_units(mean(vm_storage_vals), unit='mega')
+            try:
+                slots = available / avg_vm_datastore_usage
+                cluster_datastore_slots[cluster.name] = dict(slots=slots, metric=avg_vm_datastore_usage,
+                                                             unit='MB', type='cluster-datastore-slots')
+            except ZeroDivisionError:
+                cluster_datastore_slots[cluster.name] = dict(slots='unlimited', metric=None, unit=None,
+                                                             type='cluster-datastore-slots')
+        return cluster_datastore_slots
+
+    def print_vm_details(self, vm, interval=1, vchtime=None):
         if vchtime is None:
             vchtime = self.db_conn.CurrentTime()
         statInt = interval * 3  # There are 3 20s samples in each minute
@@ -173,43 +354,43 @@ class VcenterApi(object):
                                                              each_vm_hardware.macAddress))
 
         #CPU Ready Average
-        statCpuReady = self.build_query(vchtime, (self.stat_check('cpu.ready.summation')),
+        statCpuReady = self.build_perf_query(vchtime, (self.stat_check('cpu.ready.summation')),
                                         "", vm, interval)
         cpuReady = (float(sum(statCpuReady[0].value[0].value)) / statInt)
         #CPU Usage Average % - NOTE: values are type LONG so needs divided by 100 for percentage
-        statCpuUsage = self.build_query(vchtime, (self.stat_check('cpu.usage.average')), "", vm, interval)
+        statCpuUsage = self.build_perf_query(vchtime, (self.stat_check('cpu.usage.average')), "", vm, interval)
         cpuUsage = ((float(sum(statCpuUsage[0].value[0].value)) / statInt) / 100)
         #Memory Active Average MB
-        statMemoryActive = self.build_query(vchtime, (self.stat_check('mem.active.average')), "", vm, interval)
+        statMemoryActive = self.build_perf_query(vchtime, (self.stat_check('mem.active.average')), "", vm, interval)
         memoryActive = (float(sum(statMemoryActive[0].value[0].value) / 1024) / statInt)
         #Memory Shared
-        statMemoryShared = self.build_query(vchtime, (self.stat_check('mem.shared.average')), "", vm, interval)
+        statMemoryShared = self.build_perf_query(vchtime, (self.stat_check('mem.shared.average')), "", vm, interval)
         memoryShared = (float(sum(statMemoryShared[0].value[0].value) / 1024) / statInt)
         #Memory Balloon
-        statMemoryBalloon = self.build_query(vchtime, (self.stat_check('mem.vmmemctl.average')), "", vm, interval)
+        statMemoryBalloon = self.build_perf_query(vchtime, (self.stat_check('mem.vmmemctl.average')), "", vm, interval)
         memoryBalloon = (float(sum(statMemoryBalloon[0].value[0].value) / 1024) / statInt)
         #Memory Swapped
-        statMemorySwapped = self.build_query(vchtime, (self.stat_check('mem.swapped.average')), "", vm, interval)
+        statMemorySwapped = self.build_perf_query(vchtime, (self.stat_check('mem.swapped.average')), "", vm, interval)
         memorySwapped = (float(sum(statMemorySwapped[0].value[0].value) / 1024) / statInt)
         #Datastore Average IO
-        statDatastoreIoRead = self.build_query(vchtime, (self.stat_check('datastore.numberReadAveraged.average')),
+        statDatastoreIoRead = self.build_perf_query(vchtime, (self.stat_check('datastore.numberReadAveraged.average')),
                                          "*", vm, interval)
         DatastoreIoRead = (float(sum(statDatastoreIoRead[0].value[0].value)) / statInt)
-        statDatastoreIoWrite = self.build_query(vchtime, (self.stat_check('datastore.numberWriteAveraged.average')),
+        statDatastoreIoWrite = self.build_perf_query(vchtime, (self.stat_check('datastore.numberWriteAveraged.average')),
                                           "*", vm, interval)
         DatastoreIoWrite = (float(sum(statDatastoreIoWrite[0].value[0].value)) / statInt)
         #Datastore Average Latency
-        statDatastoreLatRead = self.build_query(vchtime, (self.stat_check('datastore.totalReadLatency.average')),
+        statDatastoreLatRead = self.build_perf_query(vchtime, (self.stat_check('datastore.totalReadLatency.average')),
                                           "*", vm, interval)
         DatastoreLatRead = (float(sum(statDatastoreLatRead[0].value[0].value)) / statInt)
-        statDatastoreLatWrite = self.build_query(vchtime, (self.stat_check('datastore.totalWriteLatency.average')),
+        statDatastoreLatWrite = self.build_perf_query(vchtime, (self.stat_check('datastore.totalWriteLatency.average')),
                                            "*", vm, interval)
         DatastoreLatWrite = (float(sum(statDatastoreLatWrite[0].value[0].value)) / statInt)
 
         #Network usage (Tx/Rx)
-        statNetworkTx = self.build_query(vchtime, (self.stat_check('net.transmitted.average')), "", vm, interval)
+        statNetworkTx = self.build_perf_query(vchtime, (self.stat_check('net.transmitted.average')), "", vm, interval)
         networkTx = (float(sum(statNetworkTx[0].value[0].value) * 8 / 1024) / statInt)
-        statNetworkRx = self.build_query(vchtime, (self.stat_check( 'net.received.average')), "", vm, interval)
+        statNetworkRx = self.build_perf_query(vchtime, (self.stat_check('net.received.average')), "", vm, interval)
         networkRx = (float(sum(statNetworkRx[0].value[0].value) * 8 / 1024) / statInt)
 
         print('\nNOTE: Any VM statistics are averages of the last {} minutes\n'.format(statInt / 3))
@@ -269,7 +450,6 @@ class VcenterApi(object):
             (float(summary.runtime.host.summary.hardware.memorySize) / 1024 / 1024 / 1024)))
 
 
-
 def print_vm_info(virtual_machine):
     """
     Print information for a particular virtual machine or recurse into a
@@ -300,3 +480,27 @@ def print_vm_info(virtual_machine):
     if summary.runtime.question is not None:
         print("Question  : ", summary.runtime.question.text)
     print("")
+
+
+def get_epoch_value():
+    return (datetime.utcnow() - datetime(1970, 1, 1)).seconds
+
+
+def mean(numbers):
+    """
+    Calculates the arithmetic mean of an array of numbers
+    :param numbers:  list of number
+    """
+    vector = np.array(numbers)
+    return np.mean(vector)
+    #return float(sum(numbers)) / max(len(numbers), 1)
+
+
+def convert_byte_units(bytes, unit='mega'):
+    if unit == 'kilo':
+        return bytes / 1024
+    if unit == 'mega':
+        return bytes / 1024 / 1024
+    if unit == 'giga':
+        return bytes / 1024 / 1024 / 1024
+    return bytes
--- unit_tests.py
+++ unit_tests.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from datetime import datetime, timedelta
 from get_vSphere_data import VcenterApi
 
@@ -12,17 +13,47 @@ class TestVmwareScripts(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_get_all_vms(self):
-        vms = self.api.get_all_vms_view()
-        self.assertIsNotNone(vms)
+    # def test_get_all_vms(self):
+    #     vms = self.api.get_all_vms_view()
+    #     self.assertIsNotNone(vms)
+    #
+    # def test_get_vm_perf_properties(self):
+    #     info = self.api.get_vm_properties(props=['name', 'runtime.powerState'])
+    #     self.assertIsNotNone(info)
+    #     self.assertTrue(len(info) > 2)
+    #     #self.api.print_vm_details(self.api.get_all_vms()[0], interval=2)
+    #
+    # def test_get_esxi_hosts_info(self):
+    #     data = self.api.build_esxi_hosts_cap_reccord()
+    #     self.assertEqual(len(data), 3)
 
-    # def test_get_vm_performance_stats(self):
-    #     vm_perf_stats = get_vm_perf_stats(self.db_conn, get_vm_by_ip(self.test_vm_ip))
-    #     self.assertIsNotNone(vm_perf_stats)
+    # def test_get_cluster_info(self):
+    #     info = self.api.build_cluster_capacity_info()
+    #     self.assertIsNotNone(info)
+    #     self.assertTrue(len(info) > 1)
+    #     self.assertIsNotNone(info[0]['name'])
+    #
+    # def test_calculate_cluster_cap_cpu_slots_available(self):
+    #     clusters = self.api.get_compute_cluster_view().view
+    #     data = self.api.get_cpu_slots_available(clusters)
+    #     print data
+    #     self.assertIsNotNone(data['SP1-Cluster-1'])
+    #
+    # def test_calculate_cluster_mem_cpu_slots(self):
+    #     clusters = self.api.get_compute_cluster_view().view
+    #     data = self.api.get_memory_slots_available(clusters)
+    #     print data
 
-    def test_get_vm_perf_properties(self):
-        info = self.api.get_vm_properties(props=['name', 'runtime.powerState'])
-        self.api.print_vm_details(self.api.get_all_vms()[0], interval=1)
+    # def test_calculate_datastore_slots(self):
+    #     clusters = self.api.get_compute_cluster_view().view
+    #     data = self.api.get_datastore_slots_available(clusters)
+    #     self.assertIsNotNone(data)
+
+    def test_get_vm_info(self):
+        vms = self.api.get_all_vms()
+        self.api.print_vm_details(vms[0])
+
+
 
 if __name__ == '__main__':
     unittest.main()
